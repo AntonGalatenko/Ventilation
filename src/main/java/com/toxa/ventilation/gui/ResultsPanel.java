7fa207b2af3deede488df02db71ae7f3825e4864
AntonGalatenko <slu4ai@gmail.com>
2016-03-24 17:59:53 +0200

    add getters and setters in ResultPanel

--- src/main/java/com/toxa/ventilation/gui/ResultsPanel.java
+++ src/main/java/com/toxa/ventilation/gui/ResultsPanel.java
@@ -13,22 +13,23 @@ public class ResultsPanel extends JPanel{
     private JPanel mainPanel;
     private JPanel fan50Panel;
     private JRadioButton fan50RadioButton;
-    public JSpinner fan50Spinner;
+    private JSpinner fan50Spinner;
+    private JComboBox fan50ComboBox;
+    private JCheckBox fan50CheckBox;
     private JRadioButton fan36RadioButton;
-    private JSpinner fan36spinner;
+    private JSpinner fan36Spinner;
     private JPanel fan36Panel;
+    private JComboBox fan36ComboBox;
     private JPanel fan26Panel;
     private JRadioButton fan26RadioButton;
     private JSpinner fan26Spinner;
-    private JPanel fanRoofPanel;
-    private JComboBox fan50ComboBox;
-    private JComboBox fan36ComboBox;
     private JComboBox fan26ComboBox;
-    private JRadioButton fanFoorRadioButton;
+    private JPanel fanRoofPanel;
+    private JRadioButton fanRoofRadioButton;
     private JSpinner fanRoofSpinner;
     private JComboBox fanRoofComboBox;
     private JRadioButton airInletSmallRadioButton;
-    private JPanel airInletSmallPanell;
+    private JPanel airInletSmallPanel;
     private JSpinner airInletSmallSpinner;
     private JComboBox airInletSmallComboBox;
     private JPanel airInletBigPanel;
@@ -41,25 +42,24 @@ public class ResultsPanel extends JPanel{
     private JComboBox shutterComboBox;
     private JPanel humidityPanel;
     private JRadioButton humidityRadioButton;
-    private JSpinner humidityLengthSpiner1;
-    private JCheckBox fan50CheckBox;
+    private JSpinner humidityLengthSpinner1;
+    private JSpinner humidityCountSpinner2;
+    private JSpinner humidityCountSpinner1;
+    private JSpinner humidityHeightSpinner;
+    private JCheckBox humidityCheckBox;
+    private JSpinner humidityLengthSpinner2;
     private JPanel heaterPanel;
     private JRadioButton heaterRadioButton;
     private JSpinner heaterSpinner;
     private JComboBox heaterComboBox;
     private JPanel fanCirculationPanel;
     private JRadioButton fanCirculationRadioButton;
-    private JSpinner fanCirculationSpiner;
+    private JSpinner fanCirculationSpinner;
     private JComboBox fanCirculationComboBox;
     private JPanel automaticPanel;
     private JRadioButton automaticRadioButton;
     private JSpinner automaticSpinner;
     private JComboBox automaticComboBox;
-    private JSpinner humidityCountSpiner2;
-    private JSpinner humidityCountSpiner1;
-    private JSpinner humidityHeightSpiner;
-    private JCheckBox humidityCheckBox;
-    private JSpinner humidityLengthSpiner2;
     private JComboBox servomotorComboBox;
     private JSpinner servomotorSpinner;
     private JSpinner emergencySpinner;
@@ -87,7 +87,196 @@ public class ResultsPanel extends JPanel{
         myMainPanel.update(this);
     }
 
-    public int getFan50spinner() {
-        return (int)(fan50Spinner.getValue());
+    public int getFan50Count() {
+        return (int) fan50Spinner.getValue();
     }
+
+    public void setFan50Count(int fan50Count) {
+        fan50Spinner.setValue(fan50Count);
+    }
+
+    public String getFan50Name() {
+        return fan50ComboBox.getSelectedItem().toString();
+    }
+
+    public boolean isFan50TwoSide() {
+        return fan50CheckBox.isSelected();
+    }
+
+    public int getAirInletBig() {
+        return (int)airInletBigSpinner.getValue();
+    }
+
+    public void setAirInletBigCount(int airInletBigCount) {
+        airInletBigSpinner.setValue(airInletBigCount);
+    }
+
+    public int getFan36Count() {
+        return (int)fan36Spinner.getValue();
+    }
+
+    public void setFan36Count(int fan36Count) {
+        fan36Spinner.setValue(fan36Count);
+    }
+
+    public String getFan36Name() {
+        return fan36ComboBox.getSelectedItem().toString();
+    }
+
+    public int getFan26Count() {
+        return (int)fan26Spinner.getValue();
+    }
+
+    public void setFan26Count(int fan26Count) {
+        fan26Spinner.setValue(fan26Count);
+    }
+
+    public String getFan26Name() {
+        return fan26ComboBox.getSelectedItem().toString();
+    }
+
+    public int getFanRoofCount() {
+        return (int)fanRoofSpinner.getValue();
+    }
+
+    public void setFanRoofCount(int fanRoofCount) {
+        fanRoofSpinner.setValue(fanRoofCount);
+    }
+
+    public String getFanRoofName() {
+        return fanRoofComboBox.getSelectedItem().toString();
+    }
+
+    public int getAirInletSmallCount() {
+        return (int)airInletSmallSpinner.getValue();
+    }
+
+    public void setAirInletSmallCount(int airInletSmallCount) {
+        airInletSmallSpinner.setValue(airInletSmallCount);
+    }
+
+    public String getAirInletSmallName() {
+        return airInletSmallComboBox.getSelectedItem().toString();
+    }
+
+    public String getAirInletBigName() {
+        return airInletBigComboBox.getSelectedItem().toString();
+    }
+
+    public int getShutterCount() {
+        return (int)shutterSpinner.getValue();
+    }
+
+    public void setShutterCount(int shutterCount) {
+        shutterSpinner.setValue(shutterCount);
+    }
+
+    public String getShutterName() {
+        return shutterComboBox.getSelectedItem().toString();
+    }
+
+    public double getHumidityLength1() {
+        return (double)humidityLengthSpinner1.getValue();
+    }
+
+    public void setHumidityLength1(double humidityLength1) {
+        humidityLengthSpinner1.setValue(humidityLength1);
+    }
+
+    public int getHumidityCount2() {
+        return (int)humidityCountSpinner2.getValue();
+    }
+
+    public void setHumidityCount2(int humidityCount2) {
+        humidityCountSpinner2.setValue(humidityCount2);
+    }
+
+    public int getHumidityCount1() {
+        return (int)humidityCountSpinner1.getValue();
+    }
+
+    public void setHumidityCount1(int humidityCount1) {
+        humidityCountSpinner1.setValue(humidityCount1);
+    }
+
+    public double getHumidityHeight() {
+        return (double)humidityHeightSpinner.getValue();
+    }
+
+    public void setHumidityHeight(double humidityHeight) {
+        humidityHeightSpinner.setValue(humidityHeight);
+    }
+
+    public boolean isHumidityCheckBox() {                                                           //изменить название
+        return humidityCheckBox.isSelected();
+    }
+
+    public double getHumidityLength2() {
+        return (double)humidityLengthSpinner2.getValue();
+    }
+
+    public void setHumidityLength2(double humidityLength2) {
+        humidityLengthSpinner2.setValue(humidityLength2);
+    }
+
+    public int getHeaterCount() {
+        return (int)heaterSpinner.getValue();
+    }
+
+    public void setHeaterCount(int heaterCount) {
+        heaterSpinner.setValue(heaterCount);
+    }
+
+    public String getHeaterName() {
+        return heaterComboBox.getSelectedItem().toString();
+    }
+
+    public int getFanCirculationCount() {
+        return (int)fanCirculationSpinner.getValue();
+    }
+
+    public void setFanCirculationCount(int fanCirculationCount) {
+        fanCirculationSpinner.setValue(fanCirculationCount);
+    }
+
+    public String getFanCirculationName() {
+        return fanCirculationComboBox.getSelectedItem().toString();
+    }
+
+    public int getAutomaticCount() {
+        return (int)automaticSpinner.getValue();
+    }
+
+    public void setAutomaticCount(int automaticCount) {
+        automaticSpinner.setValue(automaticCount);
+    }
+
+    public String getAutomaticName() {
+        return automaticComboBox.getSelectedItem().toString();
+    }
+
+    public String getServomotorName() {
+        return servomotorComboBox.getSelectedItem().toString();
+    }
+
+    public int getServomotorCount() {
+        return (int)servomotorSpinner.getValue();
+    }
+
+    public void setServomotorCount(int servomotorCount) {
+        servomotorSpinner.setValue(servomotorCount);
+    }
+
+    public int getEmergencyCount() {
+        return (int)emergencySpinner.getValue();
+    }
+
+    public void setEmergencyCount(int emergencyCount) {
+        emergencySpinner.setValue(emergencyCount);
+    }
+
+    public String getEmergencyName() {
+        return emergencyComboBox.getSelectedItem().toString();
+    }
+
 }
