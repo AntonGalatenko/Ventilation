6d184a7e792ce2e03ea249d3f4ca2dedfbd460dd
AntonGalatenko <slu4ai@gmail.com>
2016-06-20 17:54:06 +0300

    ver1.12

--- src/main/java/com/toxa/ventilation/gui/MyMainPanel.java
+++ src/main/java/com/toxa/ventilation/gui/MyMainPanel.java
@@ -8,7 +8,7 @@ public class MyMainPanel extends JFrame{
     private TaskPanel taskPanel;
     private ResultsPanel resultPanel;
     private MyToolBar toolBar;
-    private final double versionNumber = 1.11;
+    private final double versionNumber = 1.12;
 
     public MyMainPanel(){
         setTitle("Вентиляция ver" + versionNumber);
