601ae70b97a334ce379276b50b884c6f9f7d3623
AntonGalatenko <slu4ai@gmail.com>
2016-08-09 20:48:49 +0300

    finish filter by year

--- src/main/java/com/toxa/ventilation/gui/FactoryInfo.java
+++ src/main/java/com/toxa/ventilation/gui/FactoryInfo.java
@@ -8,30 +8,38 @@ import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FactoryInfo extends JDialog{
     private JPanel mainPanel;
     private JTable table1;
+    private TableModel model;
     private JScrollPane mainScrollPane;
     private JPanel yearsPanel;
     private JCheckBox checkBox2016;
     private JCheckBox checkBox2013;
     private JCheckBox checkBox2014;
     private JCheckBox checkBox2015;
+    private List<Integer> years = new ArrayList<>();
 
     public FactoryInfo(final TableModel model, final List<Factory> factoryList){
-
+        this.model = model;
         table1.setModel(model);
         setColumnSize();
 
         add(mainPanel);
 
-        setTableSorter(model);
+        checkBox2016.setSelected(true);
+        years.add(2016);
+
+        setTableSorter();
 
         setPreferredSize(new Dimension(700, getHeight(table1.getRowCount())));
         setVisible(true);
@@ -55,6 +63,56 @@ public class FactoryInfo extends JDialog{
                 openExcel(path);
             }
         });
+
+
+        checkBox2016.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED)
+                    years.add(2016);
+                else
+                    years.remove(years.indexOf(2016));
+
+                setTableSorter();
+            }
+        });
+
+        checkBox2015.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED)
+                    years.add(2015);
+                else
+                    years.remove(years.indexOf(2015));
+
+                setTableSorter();
+            }
+        });
+
+        checkBox2014.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED)
+                    years.add(2014);
+                else
+                    years.remove(years.indexOf(2014));
+
+                setTableSorter();
+            }
+        });
+
+        checkBox2013.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED)
+                    years.add(2013);
+                else
+                    years.remove(years.indexOf(2013));
+
+                setTableSorter();
+            }
+        });
+
 
     }
 
@@ -95,11 +153,19 @@ public class FactoryInfo extends JDialog{
         }
     }
 
-    private void setTableSorter(TableModel model){
-//        RowSorter<TableModel> sorter = new TableRowSorter<>(model);
-//        sorter.setr
+    private void setTableSorter(){
         TableRowSorter sorter = new TableRowSorter(model);
-        sorter.setRowFilter(RowFilter.regexFilter("2014", 0));
+
+        RowFilter<Object, Object> filter = new RowFilter<Object, Object>() {
+            @Override
+            public boolean include(Entry<?, ?> entry) {
+                Integer i = (Integer) entry.getValue(0);
+                return (years.indexOf(i.intValue()) >= 0);
+            }
+        };
+
+        sorter.setRowFilter(filter);
+        sorter.setSortsOnUpdates(true);
         table1.setRowSorter(sorter);
     }
 }
