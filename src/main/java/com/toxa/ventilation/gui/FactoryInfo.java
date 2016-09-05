dd7bbad4d3147a98689b8cef9ad17fb9a4174259
AntonGalatenko <slu4ai@gmail.com>
2016-09-05 23:16:46 +0300

    add esc ketListener add location to mainFrame

--- src/main/java/com/toxa/ventilation/gui/FactoryInfo.java
+++ src/main/java/com/toxa/ventilation/gui/FactoryInfo.java
@@ -105,6 +105,20 @@ public class FactoryInfo extends JDialog{
                 saveActualValue();
             }
         });
+
+        addEscapeListener(this);
+    }
+
+    public static void addEscapeListener(final JDialog dialog){
+        ActionListener escListener = new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e){
+                dialog.dispose();
+            }
+        };
+
+        dialog.getRootPane().registerKeyboardAction(escListener, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
     }
 
     public void setLocationForThisFrame(){
--- src/main/java/com/toxa/ventilation/gui/SettingsPanel.java
+++ src/main/java/com/toxa/ventilation/gui/SettingsPanel.java
@@ -10,6 +10,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -141,6 +142,9 @@ public class SettingsPanel extends JDialog{
                 fileChooserLabel.setText(fileChooser.getPath());
             }
         });
+
+        addEscapeListener(this);
+
     }
 
     public void setDefaultNamesDescriptionsCapacity(){
@@ -231,6 +235,18 @@ public class SettingsPanel extends JDialog{
     public void setLocationForThisFrame(){
         Point point = Main.getMainPanelLocation();
         setLocation((int)point.getX() + 10, (int)point.getY() + 30);
+    }
+
+    public static void addEscapeListener(final JDialog dialog){
+        ActionListener escListener = new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e){
+                dialog.dispose();
+            }
+        };
+
+        dialog.getRootPane().registerKeyboardAction(escListener, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
     }
 
     public String getFilePath(){
